b914ab2aa8569bae3d21c0f38e72dc021c5aef21
Nikitereh <99133872+Nikitereh@users.noreply.github.com>
2024-01-18 09:37:18 +0300

    init

---
+++ pages/product/[id].vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="product">
+        <h3 class="product__title">
+            {{ $route.params.id }}
+        </h3>
+
+        <NuxtLink
+            class="link--underline"
+            to="/products"
+        >
+            Catalogue
+        </NuxtLink>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.product {
+    &__title {
+        margin-bottom: 40px;
+    }
+}
+</style>
---
+++ pages/products.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="products">
+        <h3 class="products__header">
+            {{ title }}
+        </h3>
+
+        <div class="products__content">
+            <NuxtLink
+                v-for="item in items"
+                :key="item.id"
+                :to="'/product/' + item.id"
+                class="products__item link--underline"
+            >
+                {{ item.name }}
+            </NuxtLink>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+const title = ref('Products');
+
+const items = ref([
+    {id: 1, name: 'item1'},
+    {id: 2, name: 'item2'},
+    {id: 3, name: 'item3'},
+    {id: 4, name: 'item4'},
+]);
+</script>
+
+<style lang="scss" scoped>
+.products {
+    &__header {
+        margin-bottom: 40px;
+    }
+
+    &__content {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+    }
+
+    &__item {
+        display: inline-flex;
+        cursor: pointer;
+        transition: .15s;
+
+        &:not(:last-child) {
+            margin-bottom: 10px;
+        }
+    }
+}
+</style>
