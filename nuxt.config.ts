0de4c82bfb8dcf91d16b356f1a413babce7b00b5
Nikitereh <99133872+Nikitereh@users.noreply.github.com>
2024-02-05 10:37:47 +0300

    add auto deploy to gh pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    app: {
+        baseURL: '/Quasar-films/quasar/'
+    },
     devtools: { enabled: false },
     css: ['~/assets/scss/main.scss'],
     // runtimeConfig: {
